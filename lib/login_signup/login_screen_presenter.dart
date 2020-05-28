5d767cbef3125c784d70b0635aefde56dcf333ff
Volodymyr <vova.ostapiuk@gmail.com>
2020-05-28 17:16:28 +0300

    First working version

---
+++ lib/login_signup/login_screen_presenter.dart
@@ -0,0 +1,21 @@
+import 'package:smart_home_app/models/rest_ds.dart';
+import 'package:smart_home_app/models/user_data.dart';
+abstract class LoginScreenContract {
+  void onLoginSuccess(User user);
+  void onLoginError(String errorTxt);
+}
+
+class LoginScreenPresenter {
+  LoginScreenContract _view;
+  RestDatasource api = new RestDatasource();
+  LoginScreenPresenter(this._view);
+
+  doLogin(String email, String password) async{
+    try {
+      var user = await api.login(email, password);
+      _view.onLoginSuccess(user);
+    } on Exception catch(error) {
+      _view.onLoginError(error.toString());
+    }
+  }
+}
